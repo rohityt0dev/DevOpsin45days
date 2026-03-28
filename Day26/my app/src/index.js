99b1a6e976d86e550513ddd1219324fdf73bcc3b
rohityt0dev <rohityt0@gmail.com>
2026-03-28 13:41:25 +0530

    Set up Express server with a basic route

--- Day26/my app/src/index.js
+++ Day26/my app/src/index.js
@@ -1,1 +1,13 @@
 
+// src/index.js
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 Multi-stage Docker app running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
